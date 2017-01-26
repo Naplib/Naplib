faa82eb99ece25b6dc34ccebf7c81301ded8ff1c
baharkh22 <bahar.khalighinejad@gmail.com>
2017-01-26 15:14:53 -0500

    Add files via upload

---
+++ Matlab/Preprocessing/EEGExtract2to15.m
@@ -0,0 +1,30 @@
+function dh2 = EEGExtract2to15 (d, infs, outfs, z_score)
+
+% Requisite: Downlaod EEGfilt from EEGLab Library ::
+% https://sccn.ucsd.edu/svn/software/eeglab/functions/sigprocfunc/eegfilt.m
+
+% d: recorded data, channel x sample
+% fs: sampling rate of the data
+% outfs: sampling rate of output
+% z_score: z_score flag. In the case of z_score = 1 data will be z_scored 
+
+% Bahar, bahar.kh@columbia.edu
+
+
+defaultfs = 100; % Hz
+
+if infs ~= defaultfs
+    d = resample(d',defaultfs,infs)';
+    fs = defaultfs;
+end
+
+if exist('z_score') & z_score==1
+    signalnew=(signal(:)-repmat(mean(signal,2),1,size(signal,2)))...
+        ./repmat(std(signal(500:end)),1,size(signal,2));
+end
+
+filtered_data=eegfilt(signalnew',f,2,15,0,100);
+
+dh2 = resample(filtered_data',outfs,fs)';
+
+end
